abf49347b935dd6fe5753d04386457c936820d18
Aleksi <a12345@live.fi>
2020-03-17 14:43:25 +0200

    Created components
    - noteform, notelist, note and topbar components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteformComponent } from './noteform/noteform.component';
+import { NotelistComponent } from './notelist/notelist.component';
+import { NoteComponent } from './note/note.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteformComponent,
+    NotelistComponent,
+    NoteComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/noteform/noteform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-noteform',
+  templateUrl: './noteform.component.html',
+  styleUrls: ['./noteform.component.css']
+})
+export class NoteformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
